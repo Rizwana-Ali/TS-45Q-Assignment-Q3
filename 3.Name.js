fd7fb1e706488ced6d24c5274f4c993ed794c9ca
Rizwana Ali <160421362+Rizwana-Ali@users.noreply.github.com>
2024-03-19 08:44:20 -0700

    Add files via upload

---
+++ 3.Name.js
@@ -0,0 +1,5 @@
+let personName = "Haya";
+console.log("LowerCase:" + personName.toLowerCase());
+console.log("UpperCase:" + personName.toUpperCase());
+console.log("TitleCase:" + personName.charAt(0).toUpperCase() + personName.slice(1, 4));
+export {};
---
+++ 3.Name.ts
@@ -0,0 +1,7 @@
+let personName:string ="Haya";
+
+console.log("LowerCase:" + personName.toLowerCase());
+
+console.log("UpperCase:" + personName.toUpperCase());
+
+console.log("TitleCase:" + personName.charAt(0).toUpperCase() + personName.slice(1,4));
